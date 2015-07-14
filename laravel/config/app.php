f727638a33e2753208b84ae7d90d994babdb89b1
Mason Wood <woodrm@email.sc.edu>
2015-07-14 14:04:36 -0400

    Remove Service Provider

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -144,7 +144,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+       
 
     ],
 
